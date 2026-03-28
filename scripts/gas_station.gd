01596ffe69fc27c043dd976eda45d27b4650f6c7
Jimmy Lord <jimmylord@gmail.com>
2026-03-28 19:39:27 -0400

    Gas station zone shader.

--- scripts/gas_station.gd
+++ scripts/gas_station.gd
@@ -10,6 +10,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	var player := get_node("/root/World/Player") as Player
+	var player_pos := player.transform.origin
+	var distance_to_player := transform.origin.distance_to(player_pos)
+	var height := pow(distance_to_player, 1) * 0.3
+	var mesh_instance := get_node("pump_area/MeshInstance3D")
+	if mesh_instance:
+		var mesh := mesh_instance.mesh as Mesh;
+		var material := mesh.material as Material
+		if material:
+			material.set_shader_parameter("max_height", height)
 	pass
 
 
